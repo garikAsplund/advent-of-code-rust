1c212a593b66897a1b989a058e77485768075bcc
Garik Asplund <garik.asplund@gmail.com>
2024-01-31 17:33:05 -0800

    day3

---
+++ day-03/src/part1.rs
@@ -0,0 +1,134 @@
+use std::collections::BTreeMap;
+
+use crate::custom_error::AocError;
+
+#[derive(Debug)]
+enum Value {
+    Symbol(char),
+    Empty,
+    Number(u32),
+}
+
+#[tracing::instrument]
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    // track each line to compare above and below +/- 1 l/r
+    // if symbols are in the same line and immediately before or after
+    // then add to total
+
+    // [^A-Za-z0-9\s.] for all special symbols
+
+    let map = input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars().enumerate().map(move |(x, character)| {
+                (
+                    (y as i32, x as i32),
+                    match character {
+                        '.' => Value::Empty,
+                        c if c.is_ascii_digit() => {
+                            Value::Number(c.to_digit(10).expect("should be a number"))
+                        }
+                        c => Value::Symbol(c),
+                    },
+                )
+            })
+        })
+        .collect::<BTreeMap<(i32, i32), Value>>();
+
+    // println!("{:?}", input);
+    let mut numbers: Vec<Vec<((usize, usize), u32)>> = vec![];
+    for ((y, x), value) in map.iter() {
+        if let Value::Number(num) = value {
+            match numbers.iter().last() {
+                Some(v) => {
+                    let last_num = v.iter().last();
+                    match last_num {
+                        Some(((last_num_x, _), _)) => {
+                            if last_num_x + 1 == *x {
+                                let last = numbers.iter_mut().last().expect("should exist");
+                                last.push(((*x, *y), *num));
+                            } else {
+                                numbers.push(vec![((*x, *y), *num)]);
+                            }
+                        }
+                        None => unimplemented!("shouldn't happen"),
+                    }
+                }
+                None => numbers.push(vec![((*x, *y), *num)]),
+            }
+            // println!("{x},{y}")
+        }
+    }
+    let mut total = 0;
+    for num_list in numbers {
+        let positions = [
+            (1, 0),
+            (1, -1),
+            (0, -1),
+            (-1, -1),
+            (-1, 0),
+            (-1, 1),
+            (0, 1),
+            (1, 1),
+        ];
+
+        let num_positions: Vec<(i32, i32)> = num_list
+            .iter()
+            .map(|((y, x), _)| (*x as i32, *y as i32))
+            .collect();
+        let pos_to_check: Vec<(i32, i32)> = num_list
+            .iter()
+            .flat_map(|(pos, _)| {
+                positions
+                    .iter()
+                    .map(|outer_pos| (outer_pos.0 + pos.1 as i32, outer_pos.1 + pos.0 as i32))
+            })
+            .filter(|num| !num_positions.contains(num))
+            .collect();
+
+        dbg!(pos_to_check.len(), pos_to_check);
+
+        let is_part_number = pos_to_check.iter().any(|pos| {
+            let value = map.get(&pos);
+            if let Some(Value::Symbol(_)) = value {
+                true
+            } else {
+                false
+            }
+        });
+
+        if is_part_number {
+            total += num_list
+                .iter()
+                .map(|(_, num)| num.to_string())
+                .collect::<String>()
+                .parse::<u32>()
+                .unwrap()
+        }
+    }
+    // dbg!(numbers);
+
+    Ok("".to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "467..114..
+         ...*......
+         ..35..633.
+         ......#...
+         617*......
+         .....+.58.
+         ..592.....
+         ......755.
+         ...$.*....
+         .664.598..";
+        assert_eq!("4361", process(input)?);
+        Ok(())
+    }
+}
